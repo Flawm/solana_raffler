04da7772e17e8cf41d7f64d6ea61cd07464c034b
vlawmz <flawmz@gmail.com>
2022-05-19 07:28:11 -0600

    Add security

--- programs/raffler_anchor/src/lib.rs
+++ programs/raffler_anchor/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod error;
 pub mod state;
+pub mod security;
 
 use {
     anchor_lang::prelude::*,
---
+++ programs/raffler_anchor/src/security.rs
@@ -0,0 +1,8 @@
+use solana_security_txt::security_txt;
+
+security_txt! {
+    name: "Solana Raffler",
+    project_url: "https://github.com/Flawm/solana_raffler/",
+    contacts: "discord:VLAWMZ#1337",
+    policy: "https://github.com/Flawm/solana_raffler/README.md"
+}
