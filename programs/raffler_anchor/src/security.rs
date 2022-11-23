73b0df75690b156c315d9999c2be732f1685b3cc
Tim Schmidt <tim@neodyme.io>
2022-11-23 21:26:13 +0100

    exclude security_txt from no-entrypoint builds

--- programs/raffler_anchor/src/security.rs
+++ programs/raffler_anchor/src/security.rs
@@ -1,5 +1,6 @@
 use solana_security_txt::security_txt;
 
+#[cfg(not(feature = "no-entrypoint"))]
 security_txt! {
     name: "Solana Raffler",
     project_url: "https://github.com/Flawm/solana_raffler/",
